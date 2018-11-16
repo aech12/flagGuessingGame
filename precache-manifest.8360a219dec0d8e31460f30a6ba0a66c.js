d3347be4570c62e9e1cd3395e513c7472f92468f
Alejandro Howez <aech-12@hotmail.com>
2018-11-16 20:29:30 -0300

    Updates

---
+++ precache-manifest.8360a219dec0d8e31460f30a6ba0a66c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11d531f5dfceb9e893e7",
+    "url": "/flagGuessingGame/static/js/runtime~main.11d531f5.js"
+  },
+  {
+    "revision": "f29cc28a7bed528eaf0a",
+    "url": "/flagGuessingGame/static/js/main.f29cc28a.chunk.js"
+  },
+  {
+    "revision": "8df7078f39324016d415",
+    "url": "/flagGuessingGame/static/js/1.8df7078f.chunk.js"
+  },
+  {
+    "revision": "f29cc28a7bed528eaf0a",
+    "url": "/flagGuessingGame/static/css/main.d80cf70a.chunk.css"
+  },
+  {
+    "revision": "d747409095f7d5a61e99e5fbbcf0a696",
+    "url": "/flagGuessingGame/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/flagGuessingGame/precache-manifest.8360a219dec0d8e31460f30a6ba0a66c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/flagGuessingGame/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
